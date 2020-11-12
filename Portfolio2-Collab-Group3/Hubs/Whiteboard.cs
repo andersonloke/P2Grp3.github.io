69640f67dabf78271308502991371ca83e491ec6
Loong Kiat Lee <s10195514@connect.np.edu.sg>
2020-11-12 12:14:41 +0800

    whiteboard

---
+++ Portfolio2-Collab-Group3/Hubs/Whiteboard.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio2_Collab_Group3.Hubs
+{
+    public class Whiteboard:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
