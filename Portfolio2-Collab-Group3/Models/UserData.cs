0ab6b35994860617ea0b535fdcea6401beec7a31
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-11-13 09:35:09 +0800

    transfer data from old project

---
+++ Portfolio2-Collab-Group3/Models/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio2_Collab_Group3.Models
+{
+    public class UserData
+    {
+        public string TimestampUtc { get; set; }
+
+        public string userFirstName { get; set; }
+
+        public string userLastName { get; set; }
+
+        public string userEmail { get; set; }
+
+        public string userPwd { get; set; }
+    }
+}
